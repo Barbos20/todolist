ed02231ac2d077fba5cfa6992c16db7f42f8ba9e
Arthur Korol <artur.korol.2000.10.25@mail.ru>
2022-03-10 15:51:27 +0300

    LogIn upgrade

--- src/features/TodolistsList/Login/Login.tsx
+++ src/features/TodolistsList/Login/Login.tsx
@@ -10,7 +10,22 @@ import Button from '@mui/material/Button';
 import { useFormik } from 'formik';
 
 export const Login = () => {
+
   const formik = useFormik({
+
+    validate: (values) => {
+      if (!values.email) {
+        return {
+          email: 'email is required'
+        }
+      }
+      if (!values.password) {
+        return {
+          password: 'password is required'
+        }
+      }
+    },
+
     initialValues: {
       email: '',
       password: '',
@@ -43,17 +58,28 @@ export const Login = () => {
             </FormLabel>
             <FormGroup>
               <TextField
-                label="Email"
+                label='Email' 
                 margin="normal"
                 {...formik.getFieldProps('email')}
               />
+              {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+              
               <TextField
                 type="password"
                 label="Password"
                 margin="normal"
                 {...formik.getFieldProps('password')}
               />
-              <FormControlLabel label={'Remember me'} control={<Checkbox {...formik.getFieldProps('rememberMe')} checked={formik.values.rememberMe}/>} />
+              {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+              <FormControlLabel
+                label={'Remember me'}
+                control={
+                  <Checkbox
+                    {...formik.getFieldProps('rememberMe')}
+                    checked={formik.values.rememberMe}
+                  />
+                }
+              />
               <Button type={'submit'} variant={'contained'} color={'primary'}>
                 Login
               </Button>
