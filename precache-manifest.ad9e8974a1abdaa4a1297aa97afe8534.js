98e783fd339fa59c5fb25ea82fd925d933b580c1
Barbos20 <92588487+Barbos20@users.noreply.github.com>
2022-05-20 17:14:33 +0300

    Updates

---
+++ precache-manifest.ad9e8974a1abdaa4a1297aa97afe8534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106496dbeb58c3d1c15287a85cb84549",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "2b75371084cd011c8f1d",
+    "url": "/todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "bfa48c8c0a258cde602a",
+    "url": "/todolist/static/js/2.9ed59d1c.chunk.js"
+  },
+  {
+    "revision": "b38dbb22c32ecdedb4e97ae04f4976bc",
+    "url": "/todolist/static/js/2.9ed59d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b75371084cd011c8f1d",
+    "url": "/todolist/static/js/main.99826d1d.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
